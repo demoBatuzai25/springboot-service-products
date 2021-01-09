5c52763a775b7d6c527e564d50e74a4bc25f9d0a
Batuzai <Adelmomartinez25@gmail.com>
2021-01-08 22:43:19 -0600

    2da prueba

--- src/test/java/com/micro/api/SpringbootServiceProductsApplicationTests.java
+++ src/test/java/com/micro/api/SpringbootServiceProductsApplicationTests.java
@@ -2,7 +2,7 @@ package com.micro.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-//prueba
+
 @SpringBootTest
 class SpringbootServiceProductsApplicationTests {
 
